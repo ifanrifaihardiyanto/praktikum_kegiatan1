74415672badb97ff4d424865b905ad52bf3993cc
Ifan Rifai Hardiyanto <ifanrifai24@gmail.com>
2021-05-22 17:38:48 +0700

    add home controller & home view

---
+++ resources/views/home0200.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Ifan Rifai Hardiyanto">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+
+    <title>Ifan Rifai Hardiyanto - 1461900200 - Tugas Kegiatan 1</title>
+</head>
+
+<body>
+    <nav class="navbar">
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="artikel.html">Artikel</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact_us.html">Contact Us</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="content">
+                    <div class="card-hello">Halo, Selamat Datang!
+                        <span class="iconify" data-height="32" data-icon="emojione:waving-hand"
+                            data-inline="false"></span>
+                    </div>
+                    <div class="describe">Halaman ini adalah <strong>Tugas Kegiatan 1</strong></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/iconify.min.js') }}"></script>
+</body>
+
+</html>
